9c45efd1ff800efa31b8f9503611e5b10ba3547d
nhattminh160901 <72496975+nhattminh160901@users.noreply.github.com>
2024-04-26 14:27:46 +0700

    Update authfirebase.py

--- authfirebase.py
+++ authfirebase.py
@@ -4,19 +4,19 @@ import pyrebase
 class FirebaseAuth():
     def firebase():
         try:
-            email = "toilaminh@mail.com"
-            password = "123456"
+            email = ""
+            password = ""
 
             firebaseConfig = {
-            'apiKey': "AIzaSyCt60H3fUGiPv973_fMMN51lp2XXRazjF0",
-            'authDomain': "arduino-firebase-vippro.firebaseapp.com",
-            'databaseURL': "https://arduino-firebase-vippro-default-rtdb.firebaseio.com",
-            'projectId': "arduino-firebase-vippro",
-            'storageBucket': "arduino-firebase-vippro.appspot.com",
-            'messagingSenderId': "437474413862",
-            'appId': "1:437474413862:web:92d2b1c67528bf994fc5e7",
-            'measurementId': "G-HMRKLR9EVW",
-            'serviceAccount': "serviceAccountKey.json"
+            'apiKey': "",
+            'authDomain': "",
+            'databaseURL': "",
+            'projectId': "",
+            'storageBucket': "",
+            'messagingSenderId': "",
+            'appId': "",
+            'measurementId': "",
+            'serviceAccount': ""
             }
 
             firebase = pyrebase.initialize_app(firebaseConfig)
